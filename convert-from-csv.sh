fa0d30270bd25d3cdf3fbb4971cb1afff4c90648
mattecasu <matteo.casu@babylonhealth.com>
2019-05-10 16:35:24 +0100

    upgrading libs

--- convert-from-csv.sh
+++ convert-from-csv.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-mvn clean install
-wget -O alternateNames.zip http://download.geonames.org/export/dump/alternateNames.zip
-wget -O allCountries.zip http://download.geonames.org/export/dump/allCountries.zip
-wget -O hierarchy.zip http://download.geonames.org/export/dump/hierarchy.zip
-unzip alternateNames.zip && rm -f alternateNames.zip
-unzip allCountries.zip && rm -f allCountries.zip
-unzip hierarchy.zip && rm -f hierarchy.zip
+./gradlew clean compileJava
+echo "Downloading alternate names .."
+wget -q -O alternateNames.zip http://download.geonames.org/export/dump/alternateNames.zip
+echo "Downloading all countries file .."
+wget -q -O allCountries.zip http://download.geonames.org/export/dump/allCountries.zip
+echo "Downloading hierarchies file .."
+wget -q -O hierarchy.zip http://download.geonames.org/export/dump/hierarchy.zip
+echo "Unzipping .."
+unzip -q alternateNames.zip && rm -f alternateNames.zip
+unzip -q allCountries.zip && rm -f allCountries.zip
+unzip -q hierarchy.zip && rm -f hierarchy.zip
 mkdir -p input_source
 mv -f allCountries.txt input_source/
-mv -f alternateNames.txt input_source/ 
+mv -f alternateNames.txt input_source/
 mv -f hierarchy.txt input_source/
-mvn exec:exec -Dexec.args="-cp %classpath $MAVEN_OPTS GeonamesProducer"
+echo "Running conversion .."
+./gradlew run -DmainClass=convertion.GeonamesProducer
