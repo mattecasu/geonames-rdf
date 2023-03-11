f2f89bf9be5f09800d268b51dd9b9d19376c3f53
mattecasu <mattecasu@gmail.com>
2023-03-11 18:43:01 +0000

    geonames

--- convert-from-csv.sh
+++ convert-from-csv.sh
@@ -2,6 +2,8 @@
 
 ./gradlew clean compileJava
 echo "Downloading alternate names .."
+mkdir -p input_source
+cd input_source
 wget -q -O alternateNames.zip http://download.geonames.org/export/dump/alternateNames.zip
 echo "Downloading all countries file .."
 wget -q -O allCountries.zip http://download.geonames.org/export/dump/allCountries.zip
@@ -11,9 +13,6 @@ echo "Unzipping .."
 unzip -q alternateNames.zip && rm -f alternateNames.zip
 unzip -q allCountries.zip && rm -f allCountries.zip
 unzip -q hierarchy.zip && rm -f hierarchy.zip
-mkdir -p input_source
-mv -f allCountries.txt input_source/
-mv -f alternateNames.txt input_source/
-mv -f hierarchy.txt input_source/
+cd ..
 echo "Running conversion .."
 ./gradlew run -DmainClass=convertion.GeonamesProducer
